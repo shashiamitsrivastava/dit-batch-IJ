907e2c3c19fb54138bf09ab19a80e800d93e56ea
shashiamitsrivastava <92148217+shashiamitsrivastava@users.noreply.github.com>
2024-07-18 19:45:42 +0530

    Add files via upload

--- BSTDemo.java
+++ BSTDemo.java
@@ -17,6 +17,8 @@ class BSTOperation{
         root = insertNewNode(75,root);
         print(root);
         System.out.println("MAX "+maxValue(root));
+        remove(root, root, true, 18);
+        print(root);
         
     }
 
@@ -37,6 +39,59 @@ class BSTOperation{
        
 
     }
+
+    // for remove
+    void remove(BSTNode<Integer> currentNode, BSTNode<Integer> parent,
+     boolean isLeft, int data ){
+        if(data>currentNode.data){
+            // look up in right
+            remove(currentNode.right, currentNode, false, data);
+        }
+        else if (data< currentNode.data){
+            remove(currentNode.left, currentNode, true, data);
+        }
+        else{
+            // Data found in tree
+            // Case - 1 We are on leaf node (No Left Child , No Right Child)
+            if(currentNode.left == null && currentNode.right==null){
+                if(isLeft){
+                    // I am on Left
+                    parent.left  = null;
+                }
+                else{
+                    // I am on right
+                    parent.right = null;
+                }
+            }
+            // Case - 2 CurrentNode right!=null and left =null
+            if(currentNode.left == null && currentNode.right!=null){
+                if(isLeft){
+                    parent.left = currentNode.right;
+                }
+                else{
+                    parent.right = currentNode.right;
+                }
+            }
+            // case - 3 currentNode left!=null and right = null
+            if(currentNode.left!=null && currentNode.right==null){
+                if(isLeft){
+                    parent.left = currentNode.left;
+
+                }
+                else{
+                    parent.right = currentNode.left;
+                }
+            }
+            // case - 4 current.left!=null and currentNode.right !=null
+            if(currentNode.left!=null && currentNode.right!=null){
+                // get the left max
+                int max = maxValue(currentNode.left);
+                currentNode.data= max;
+                remove(currentNode.left, currentNode, true, max);
+            }
+        }
+    }
+
 
     BSTNode<Integer> insertNewNode(int data, BSTNode<Integer> parent){
         if(parent == null){
